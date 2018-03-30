83374234da5723b51e4f62b916ea13e98676c5e5
Immo Heikkinen <immo.heikkinen@gmail.com>
2018-03-30 19:43:42 +0300

    Add generative tests for ns form rewriting
    Closes #1

---
+++ test/nsorg/core_gen_tests.clj
@@ -0,0 +1,29 @@
+(ns nsorg.core-gen-tests
+  (:require [clojure.core.specs.alpha :as core-specs]
+            [clojure.test.check.generators :as gen]
+            [clojure.spec.alpha :as s]
+            [midje.experimental :refer [for-all]]
+            [midje.sweet :refer :all]
+            [nsorg.core :as nsorg]))
+
+;; Patch spec for :gen-class method to add generator
+(s/def ::core-specs/method (s/with-gen (s/and vector?
+                                              (s/cat :name simple-symbol?
+                                                     :param-types ::core-specs/signature
+                                                     :return-type simple-symbol?))
+                                       #(gen/fmap vec (s/gen (s/cat :name simple-symbol?
+                                                                    :param-types ::core-specs/signature
+                                                                    :return-type simple-symbol?)))))
+
+(defn has-same-length-as [expected]
+  (fn [actual]
+    (= (count actual) (count expected))))
+
+(for-all
+  [ns-form (gen/fmap
+             (fn [ns-form-args]
+               (str (cons 'ns ns-form-args)))
+             (s/gen ::core-specs/ns-form))]
+  (fact
+    "Rewrite function is able to handle arbitary ns forms"
+    (nsorg/rewrite-ns-form ns-form) => (has-same-length-as ns-form)))
