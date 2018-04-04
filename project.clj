5492966c28725f1b84d0388f11e511683f5d5447
Immo Heikkinen <immo.heikkinen@gmail.com>
2018-04-04 21:06:29 +0300

    Bump to next dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nsorg "0.1.2"
+(defproject nsorg "0.2.0-SNAPSHOT"
   :description "Clojure library for organizing ns form"
   :url "https://github.com/immoh/nsorg"
   :license {:name "Eclipse Public License"
