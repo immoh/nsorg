22d101f88474f923cc086b739013ce237cfa1b9c
Immo Heikkinen <immo.heikkinen@gmail.com>
2018-09-15 20:59:14 +0300

    Bump deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/immoh/nsorg"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[rewrite-clj "0.6.0"]]
-  :profiles {:dev {:dependencies [[midje "1.9.2-alpha1"]
+  :dependencies [[rewrite-clj "0.6.1"]]
+  :profiles {:dev {:dependencies [[midje "1.9.2"]
                                   [org.clojure/clojure "1.9.0"]]
                    :plugins      [[lein-codox "0.10.3"]
                                   [lein-midje "3.2.1"]]}}
