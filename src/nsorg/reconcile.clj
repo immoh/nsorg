e52ca18d02ebfe6fa75a5a0eb9ff1a41cdb1a9b2
Immo Heikkinen <immo.heikkinen@gmail.com>
2020-12-22 13:37:44 +0200

    WIP

--- src/nsorg/reconcile.clj
+++ src/nsorg/reconcile.clj
@@ -11,7 +11,7 @@
 (def s "
 (ns moi
   (:require
-    [koira]
+    [koira] ;; do not touch!
     [kissa]
     [elefantti]))")
 
@@ -21,12 +21,12 @@
                        :namespace 'kissa.a
                        :options   [{:key :refer
                                     :val '[foo bar]}]}
-                      {:type      :prefix-libspec
-                       :prefix    'asda
-                       :libspecs [{:type :libspec
+                      {:type     :prefix-libspec
+                       :prefix   'asda
+                       :libspecs [{:type      :libspec
                                    :namespace 'c
-                                   :options {:key :refer
-                                             :val :all}}]}]]}
+                                   :options   {:key :refer
+                                               :val :all}}]}]]}
 
 
 
@@ -35,40 +35,140 @@
 (def n (parser/parse-string-all s))
 
 
+;;;; parsing part
+
+(defn with-meta+ [obj m]
+  (with-meta obj
+             (merge (meta obj) m)))
+
 (defn ns-clause? [form]
   (and (sequential? form)
        (keyword? (first form))))
+
+
+(defn parse-sequential [zloc pred]
+  (loop [zloc (zip/down zloc)
+         done? false
+         i 0
+         children []]
+    (cond
+      done?
+      {:node     (zip/node (zip/up zloc))
+       :children children}
+
+      (pred (nzip/sexpr zloc))
+      (let [modified-zloc (zip/replace zloc [::placeholder i])]
+        (if-let [new-zloc (zip/right modified-zloc)]
+          (recur new-zloc false (inc i) (conj children (zip/node zloc)))
+          (recur modified-zloc true (inc i) (conj children (zip/node zloc)))))
+
+      :else
+      (if-let [new-zloc (zip/right zloc)]
+        (recur new-zloc false i children)
+        (recur zloc true i children)))))
+
 
 (defn parse-libspec [zloc]
   {:type      :libspec
    :namespace (zip/sexpr (zip/down zloc))})
 
+
 (defn parse-ns-clause [zloc]
-  (let [sexpr (nzip/sexpr zloc)]
-    (with-meta {:type     (first sexpr)
-                :libspecs (vec (map-indexed (fn [i zloc]
-                                              (with-meta
-                                                (parse-libspec zloc)
-                                                {::index i}))
-                                            (nzip/right-nodes (zip/right (zip/down zloc)))))}
-               {::node (zip/node zloc)})))
+  (let [{:keys [node children]} (parse-sequential zloc (complement keyword?))]
+    (with-meta+ {:type (first (nzip/sexpr zloc))
+                 :libspecs (vec (map-indexed (fn [i child]
+                                               (with-meta+ (parse-libspec (zip/edn child))
+                                                           {::index i}))
+                                             children))}
+                {::node node})))
 
 (defn parse-ns-clauses [zloc]
-  (vec (filter identity (map-indexed (fn [i zloc]
-                                       (when (ns-clause? (nzip/sexpr zloc))
-                                         (with-meta (parse-ns-clause zloc)
-                                                    {::index i})))
-                                     (nzip/right-nodes (zip/down zloc))))))
+  (let [{:keys [node children]} (parse-sequential zloc ns-clause?)]
+    (with-meta+ {:clauses (vec (map-indexed (fn [i child]
+                                              (with-meta+ (parse-ns-clause (zip/edn child))
+                                                          {::index i}))
+                                            children))}
+                {::node node})))
 
 (defn parse-ns-form [zloc]
   (let [sexpr (nzip/sexpr zloc)]
     (and (list? sexpr)
          (= (first sexpr) 'ns)
-         (with-meta {:clauses (parse-ns-clauses zloc)}
-                    {::node (zip/node zloc)}))))
+         (parse-ns-clauses zloc))))
 
-(defn node [ns-form]
-  (::node (meta ns-form)))
+;;;;;;;;;
+
+
+;;;;;; stringing part
+
+
+
+(defn placeholder-node? [form]
+  (and (vector? form)
+       (= (first form) ::placeholder)))
+
+(defn node-libspec [{:keys [namespace]}]
+  (node/coerce namespace))
+
+(defn node-ns-clause [{:keys [libspecs] :as clause}]
+  (let [remaining-indices (set (map (comp ::index meta) libspecs))]
+    (loop [zloc (zip/down (zip/edn (::node (meta clause))))
+           done? false
+           libspecs libspecs]
+      (cond
+        done?
+        (zip/node (zip/up zloc))
+
+        (placeholder-node? (nzip/sexpr zloc))
+        (let [index (second (nzip/sexpr zloc))]
+          (if (remaining-indices index)
+            ;; replaced
+            (let [modified-zloc (zip/replace zloc (node-libspec (first libspecs)))]
+              (if-let [new-zloc (zip/right modified-zloc)]
+                (recur new-zloc false (rest libspecs))
+                (recur modified-zloc true (rest libspecs))))
+            ;; deleted -- doesn't handle case of only 1 node
+            (let [modified-zloc (zip/remove zloc)]
+              (if-let [new-zloc (if (zip/leftmost? zloc)
+                                  (zip/down modified-zloc)
+                                  (zip/right modified-zloc))]
+                (recur new-zloc false libspecs)
+                (recur modified-zloc true libspecs)))))
+
+        :else
+        (if-let [new-zloc (zip/right zloc)]
+          (recur new-zloc false libspecs)
+          (recur zloc true libspecs))))))
+
+(defn node-ns-form [{:keys [clauses] :as ns-form}]
+  (let [remaining-indices (set (map (comp ::index meta) clauses))]
+    (loop [zloc (zip/down (zip/edn (::node (meta ns-form))))
+           done? false
+           clauses clauses]
+      (cond
+        done?
+        (zip/node (zip/up zloc))
+
+        (placeholder-node? (nzip/sexpr zloc))
+        (let [index (second (nzip/sexpr zloc))]
+          (if (remaining-indices index)
+            ;; replaced
+            (let [modified-zloc (zip/replace zloc (node-ns-clause (first clauses)))]
+              (if-let [new-zloc (zip/right modified-zloc)]
+                (recur new-zloc false (rest clauses))
+                (recur modified-zloc true (rest clauses))))
+            ;; deleted -- doesn't handle case of only 1 node
+            (let [modified-zloc (zip/remove zloc)]
+              (if-let [new-zloc (if (zip/leftmost? zloc)
+                                   (zip/down modified-zloc)
+                                   (zip/right modified-zloc))]
+                (recur new-zloc false clauses)
+                (recur modified-zloc true clauses)))))
+
+        :else
+        (if-let [new-zloc (zip/right zloc)]
+          (recur new-zloc false clauses)
+          (recur zloc true clauses))))))
 
 
 
